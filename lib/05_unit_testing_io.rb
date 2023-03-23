826a6b89899e43c4177002917d24f3934cf1e670
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-03-23 16:31:29 +0000

    Mocking Bites 5: Unit Testing IO - StringRepeater

--- lib/05_unit_testing_io.rb
+++ lib/05_unit_testing_io.rb
@@ -1,3 +1,46 @@
+# Challenge
+#-------------------------------
+
+
+
+# Hello. I will repeat a string many times.
+# Please enter a string
+# TWIX
+# Please enter a number of repeats
+# 10
+# Here is your result:
+# TWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIX
+
+class StringRepeater
+  def initialize(io)
+    @io = io
+  end
+
+  def run
+  @io.puts "Hello. I will repeat a string many times."
+  @io.puts "Please enter a string"
+  chocolate = @io.gets.chomp
+  @io.puts "Please enter a number of repeats"
+  repeats = @io.gets.chomp.to_i
+  @io.puts "Here is your result:"
+  @io.puts "#{chocolate * repeats}"
+  end
+
+end
+
+string_repeater = StringRepeater.new(Kernel)
+string_repeater.run
+
+
+
+
+
+=begin
+  #       Exercise
+  #-------------------------------
+rescue => exception
+  
+end
 class InteractiveCalculator
   def initialize(io)
      @io = io
@@ -26,9 +69,6 @@ interactive_calculator = InteractiveCalculator.new(Kernel)
 interactive_calculator.run
 
 
-
-
-
 # Hello. I will subtract two numbers.
 # Please enter a number
 # 4
@@ -38,16 +78,11 @@ interactive_calculator.run
 # 4 - 3 = 1
 
 
-
-
-
-
-
-
-
 #note: Kernal. is implicitly put by Ruby usually.
 # We have shown it below so we can see easily how we use doubles
 # puts "Hello. I will subtract two numbers."
 # puts "Please enter a number"
 # first_num = Kernal.gets.chomp
 # second_num = Kernal.gets.chomp
+
+=end
--- spec/05_unit_testing_io_spec.rb
+++ spec/05_unit_testing_io_spec.rb
@@ -1,5 +1,43 @@
 require '05_unit_testing_io'
 
+
+RSpec.describe StringRepeater do
+  it "Accepts a string and outputs it 10 times" do
+    io = double :io
+    expect(io).to receive(:puts).with("Hello. I will repeat a string many times.")
+    expect(io).to receive(:puts).with("Please enter a string")
+    expect(io).to receive(:gets).and_return("TWIX")
+    expect(io).to receive(:puts).with("Please enter a number of repeats")
+    expect(io).to receive(:gets).and_return("10")
+    expect(io).to receive(:puts).with("Here is your result:")
+    expect(io).to receive(:puts).with("TWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIX")
+    string_repeater = StringRepeater.new(io)
+    string_repeater.run
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
+
 RSpec.describe InteractiveCalculator do
   it "it requests two numbers and returns a calculation phrase" do
     io = double :io
@@ -14,3 +52,5 @@ RSpec.describe InteractiveCalculator do
     calc.run
   end
 end
+
+=end
