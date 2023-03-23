1105fc358bd5fb2a7ad723e40c315ccbbe86d769
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-03-23 12:06:38 +0000

    Mocking Bites 4: Exercise TimeError with API - completed

--- lib/04_unit_testing_apis.rb
+++ lib/04_unit_testing_apis.rb
@@ -1,18 +1,21 @@
 require 'date'
 require 'json'
-require 'net/http'
+#require 'net/http'
 
 class TimeError
+  def initialize(requestor)
+    @requestor = requestor
+  end
   # Returns difference in seconds between server time
   # and the time on this computer
-  def error
-    return get_server_time - Time.now
+  def error(current_time)
+    return get_server_time - current_time
   end
 
   private
 
   def get_server_time
-    text_response = Net::HTTP.get(URI("https://worldtimeapi.org/api/ip"))
+    text_response = @requestor.get(URI("https://worldtimeapi.org/api/ip"))
     json = JSON.parse(text_response)
     return DateTime.parse(json["utc_datetime"]).to_time
   end
